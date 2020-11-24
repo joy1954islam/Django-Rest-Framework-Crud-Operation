bce86e686ca7ec5511be8b7b57920aed989f31f7
joyislam1954 <joyislam1954@gmail.com>
2020-11-24 14:30:27 +0600

    Django Rest Framework Crud Operations

--- Django_FBV_Crud_Operation/FBV/models.py
+++ Django_FBV_Crud_Operation/FBV/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Student_Admission(models.Model):
+    student_id = models.CharField('Student ID',max_length=200)
+    student_age = models.IntegerField('Student Age')
+    student_name = models.CharField('Student Name',max_length=150)
+    student_email = models.EmailField('Student Email')
+    student_amount = models.FloatField('Student Amount')
+    student_address = models.TextField('Student Address')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.student_name
